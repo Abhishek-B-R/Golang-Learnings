848f71395e5443c2aad4f9c8cd564eaec41a3aa1
Abhishek B R <byteblaster1468@gmail.com>
2025-12-27 21:29:04 +0530

    added multiple names greetings simultaneously

--- 2.modules/hello/hello.go
+++ 2.modules/hello/hello.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"example.com/greetings"
+	"log"
 )
 
 func main(){
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	message, err := greetings.Hello("Abhishek")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return
 	}
 	fmt.Println(message)
+
+
+    names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(messages)
 }
